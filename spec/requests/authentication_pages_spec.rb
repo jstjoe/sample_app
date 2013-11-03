032841afebc47400eb6143eef36c4aa8640ecaee
Joseph McCarron <joe@josephmccarron.com>
2013-11-03 08:00:16 -0800

    Added signin functionality and tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,11 +7,23 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
+    before { visit signin_path }
+    
     describe "with invalid information" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
