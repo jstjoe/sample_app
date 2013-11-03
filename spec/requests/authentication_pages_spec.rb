03bccd904b649dbbc6cba1d1bbf1f5cb1b349fd1
Joseph McCarron <joe@josephmccarron.com>
2013-11-03 08:43:58 -0800

    Finish signin form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Authentication" do
 
   describe "signin" do
     before { visit signin_path }
-    
+
     describe "with invalid information" do
       before { click_button "Sign in" }
 
@@ -38,6 +38,11 @@ describe "Authentication" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
